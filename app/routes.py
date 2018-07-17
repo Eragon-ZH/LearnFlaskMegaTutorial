fd5eb553ae87f3fc1317dfc4b709a38a3babda63
Eragon-ZH <980019347@qq.com>
2018-07-17 20:20:04 +0800

    web forms

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -16,3 +17,14 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    # 如果是POST就会收集所有数据并执行验证方法，如果是GET返回False
+    if form.validate_on_submit():
+        # 向用户展示一条消息
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
