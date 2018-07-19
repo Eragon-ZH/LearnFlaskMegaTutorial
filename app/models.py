068c9e111bf1c9d852d0ec1e25c340442388df28
Eragon-ZH <980019347@qq.com>
2018-07-19 14:30:59 +0800

    Followers

--- app/models.py
+++ app/models.py
@@ -5,6 +5,12 @@ from hashlib import md5
 
 from app import db, login
 
+# followers关联表
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
 class User(UserMixin, db.Model):
     """用户"""
     """flask_login需要实现is_authenticated,is_activate,is_anonymous,get_id,
@@ -19,6 +25,18 @@ class User(UserMixin, db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    # 关注列表
+    followed = db.relationship(
+        # 自引用，左右两侧都是user，secondary指向关系的关联表(上面定义的followers)
+        'User', secondary=followers,
+        # 通过关联表关联左侧(关注者)的条件，即follower_id字段与这个user的id匹配
+        # followers.c.follower_id表达式引用了该关系表中的follower_id列
+        primaryjoin=(followers.c.follower_id == id),
+        # 通过关联表关联右侧(被关注者)的条件，
+        secondaryjoin=(followers.c.followed_id == id),
+        # backref定义右侧实体如何访问该关系。左侧followed，右侧followers
+        # lazy参数定义查询的执行方式。backref内应用于右侧，在外应用于左侧
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
     def set_password(self, password):
         """保存密码的哈希"""
@@ -33,6 +51,35 @@ class User(UserMixin, db.Model):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
             digest, size)
+
+    def follow(self, user):
+        """关注"""
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        """取消关注"""
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        """是否关注某用户"""
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
+
+    def followed_posts(self):
+        """查询关注用户的posts"""
+        # 关注用户的posts
+        followed_post = Post.query.join(
+        # 先对post进行联结(join)操作，第一个参数是关联表(followers)，第二个参数是
+        # 联结条件。会创建一个将posts表数据和followers表数据组合起来的临时表
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+            # 再过滤(filter)只剩该用户关注的用户的post
+                followers.c.follower_id == self.id)
+        # 自己的posts
+        own_post = Post.query.filter_by(user_id=self.id)
+        # 按时间降序返回
+        return followed_post.union(own_post).order_by(Post.timestamp.desc())
 
     def __repr__(self):
         """打印"""
